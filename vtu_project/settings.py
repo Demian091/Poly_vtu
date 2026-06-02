a3270bcac77ef6679da58862e9a70591657b8099
Demian091 <123770496+Demian091@users.noreply.github.com>
2026-06-02 13:59:12 +0100

    Update database configuration in settings.py

--- vtu_project/settings.py
+++ vtu_project/settings.py
@@ -89,10 +89,10 @@ LOGOUT_REDIRECT_URL = "login"
 
 DATABASES = {
     'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
