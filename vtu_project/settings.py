1f1abcf63dc0ee62ed5324abc923a6ec30938bbb
Demian091 <123770496+Demian091@users.noreply.github.com>
2026-06-08 07:31:51 +0100

    Refactor DATABASES setting for SQLite configuration

--- vtu_project/settings.py
+++ vtu_project/settings.py
@@ -136,12 +136,11 @@ LOGOUT_REDIRECT_URL = "login"
 # # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
